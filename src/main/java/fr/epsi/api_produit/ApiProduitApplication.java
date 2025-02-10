0413b2ff9e582b05b46c79691199d17900beeb60
StevenD <dellarica.steven@gmail.com>
2025-02-10 20:56:07 +0100

    First commit

---
+++ src/main/java/fr/epsi/api_produit/ApiProduitApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.api_produit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProduitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProduitApplication.class, args);
+    }
+
+}
